412eb9f649ae2ea77ba778d1b8b58aecf5355741
jroaga <juan.roa@eost.io>
2023-07-13 11:05:58 -0500

    Re-review tradingview.com iframe again to solve visual issues

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -4,7 +4,7 @@
     <iframe scrolling="no" allowtransparency="true" frameborder="0"
       src="https://s.tradingview.com/embed-widget/ticker-tape/?locale=en#%7B%22symbols%22%3A%5B%7B%22proName%22%3A%22FOREXCOM%3ASPXUSD%22%2C%22title%22%3A%22S%26P%20500%22%7D%2C%7B%22proName%22%3A%22FOREXCOM%3ANSXUSD%22%2C%22title%22%3A%22US%20100%22%7D%2C%7B%22proName%22%3A%22FX_IDC%3AEURUSD%22%2C%22title%22%3A%22EUR%2FUSD%22%7D%2C%7B%22proName%22%3A%22BITSTAMP%3ABTCUSD%22%2C%22title%22%3A%22Bitcoin%22%7D%2C%7B%22proName%22%3A%22BITSTAMP%3AETHUSD%22%2C%22title%22%3A%22Ethereum%22%7D%5D%2C%22showSymbolLogo%22%3Atrue%2C%22colorTheme%22%3A%22dark%22%2C%22isTransparent%22%3Atrue%2C%22displayMode%22%3A%22adaptive%22%2C%22width%22%3A%22100%25%22%2C%22height%22%3A76%2C%22utm_source%22%3A%22www.tradingview.com%22%2C%22utm_medium%22%3A%22widget_new%22%2C%22utm_campaign%22%3A%22ticker-tape%22%2C%22page-uri%22%3A%22www.tradingview.com%2Fwidget%2Fticker-tape%2F%22%7D"
       style="box-sizing: border-box; display: block;  width: 100%"></iframe>
-    <h2 class="h2-bold">Dashboard</h2>
+    <h2 class="h2-bold">{{ $t('dashboard.title') }}</h2>
     <!-- Banner -->
     <article class="dashboard__banner">
       <div class="dashboard__banner-box">
@@ -144,13 +144,9 @@ import ChartDonut from "./charts/ChartDonut.vue";
 
 <style lang="scss" scoped>
 iframe {
-  height: 44px;
-}
-
-@media (max-width: 834px) {
-  iframe {
-    height: 50px;
-  }
+  height: 50px;
+  display: flex !important;
+  align-items: center;
 }
 
 .dashboard {
